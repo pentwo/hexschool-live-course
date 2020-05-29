ec586dd4211fb590dccc60f99c84c5b27e4b6e1d
pentwo <pentwo@gmail.com>
2020-05-29 09:06:53 +0800

    05/29 index page with Animate on Scroll (navbar still working)

---
+++ week-6/gulpfile.js/envOptions.js
@@ -0,0 +1,38 @@
+const srcPath = './app';
+const distPath = './dist';
+
+let envOptions = {
+  string: 'env',
+  default: {
+    env: 'dev',
+  },
+  conyFile: {
+    src: [
+      `${srcPath}/**/*`,
+      `!${srcPath}/assets/style/**/*.scss`,
+      `!${srcPath}/assets/style/**/*.sass`,
+      `!${srcPath}/**/*.ejs`,
+      `!${srcPath}/**/*.html`,
+    ],
+    path: distPath,
+  },
+  html: {
+    src: [`${srcPath}/**/*.html`,],
+    ejsSrc: [`${srcPath}/**/*.ejs`,],
+    path: distPath,
+  },
+  style: {
+    src: [
+      `${srcPath}/assets/style/**/*.scss`,
+      `${srcPath}/assets/style/**/*.sass`,
+    ],
+    path: `${distPath}/assets/style`,
+  },
+  clean: {
+    src: distPath,
+  },
+  browserDir: distPath,
+  deploySrc: `${distPath}/**/*`,
+};
+
+exports.envOptions = envOptions;
